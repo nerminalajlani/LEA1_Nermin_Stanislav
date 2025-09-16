9b24e7b465831c22308b25467eb96a470cac5ef5
nerminalajlani <Nermin.Alajlani@edu.bib.de>
2025-09-16 14:03:26 +0200

    Update Sender.ino

--- Sender.ino
+++ Sender.ino
@@ -1,21 +1,27 @@
+String message;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Serial3.begin(9600);
-  Serial.setTimeout(50);
-  Serial1.setTimeout(50);
 
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
+
+  Serial.println("Setup abgeschlossen. Bitte Nachricht eingeben...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available())
-  {
-    messege = Serial.readString();
-    Serial3.print();
+  // Wenn Daten vom PC (Serial) kommen → an Serial3 weiterleiten
+  if (Serial.available()) {
+    message = Serial.readStringUntil('\n'); // bis Zeilenende lesen
+    Serial3.println(message);               // mit Zeilenumbruch senden
+  }
+
+  // Wenn Daten von Serial3 kommen → an PC (Serial) weiterleiten
+  if (Serial3.available()) {
+    message = Serial3.readStringUntil('\n'); 
+    Serial.println(message);
   }
 }
